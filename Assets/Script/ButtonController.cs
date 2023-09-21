e499929cab91f3676d4205a14eb27dc8616c177b
MartinJ-Laptop <martinj@educ.college-montreal.qc.ca>
2023-09-20 20:55:57 -0400

    Ajouter 2 boutons, Pouvoir placer 3 objets
    on peut maintenant placer un cube, un cylindre et une capsule a laide de 3 boutons

---
+++ Assets/Script/ButtonController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingManager : MonoBehaviour
+{
+    public GameObject[] objects;
+    private GameObject object_loading;
+    private Vector3 position;
+    private RaycastHit hits_ground;
+    [SerializeField] private LayerMask layerMask;
+
+    void Update()
+    {
+        if (object_loading != null)
+        {   
+            object_loading.transform.position = position;
+            
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                object_loading = null;
+            }
+        }
+        
+    }
+    
+
+    private void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hits_ground, 1000, layerMask))
+        {
+           position = hits_ground.point;
+        }   
+    }
+
+    public void SelectObject(int index)
+    {
+        object_loading = Instantiate(objects[index], position, transform.rotation);
+        //object_loading.SetActive(false);
+    }
+
+}
