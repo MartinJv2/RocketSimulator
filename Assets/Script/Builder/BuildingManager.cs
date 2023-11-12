ec55e93144ce5d864310a0d90400d5186dde6998
Alexandre Cabana <cabanaA@educ.college-montreal.qc.ca>
2023-11-12 10:30:38 -0500

    Mouving system make in one script and add cone
    1)Transfer the script for mouving component of buildingmanager to edit place object
    2)Add cone
    3)Create a textures folder

--- Assets/Script/Builder/BuildingManager.cs
+++ Assets/Script/Builder/BuildingManager.cs
@@ -1,99 +1,20 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BuildingManager : MonoBehaviour
 {
     public GameObject[] objects;
-    public GameObject Parent;
-    public GameObject object_loading;
-    private Vector3 position;
-    private RaycastHit hits_ground;
-    public float gridSize;
-    [SerializeField] private LayerMask LayerMask;
-    public bool canPlace;
-    [SerializeField] private Material[] showingplaceable;
-    private bool placed = false;
-    
-    void Update()
-    {
-        if (object_loading != null)
-        {
-            object_loading.transform.position = new Vector3(
-                Gridsnap(position.x),
-                Gridsnap(position.y),
-                Gridsnap(position.z));
-            Collider[] hitColliders = Physics.OverlapSphere(new Vector3(Gridsnap(position.x), Gridsnap(position.y), Gridsnap(position.z)), 0.6f);
-            Collider[] minihitcollider = Physics.OverlapSphere(new Vector3(Gridsnap(position.x), Gridsnap(position.y + 0.1f), Gridsnap(position.z)), 0.0001f);
-            if (hitColliders.Length <= 2 && placed)
-            {
-                canPlace = false;
-            }
-            else
-            {
-                canPlace = true;
-            }
-            UpdateColor();
-            
-
-            if (Input.GetMouseButtonDown(0) && canPlace && hitColliders.Length >= 2 && minihitcollider.Length==2)
-            {
-                object_loading.GetComponent<MeshRenderer>().material = showingplaceable[2];
-                object_loading = null;
-                canPlace = false;
-                if(placed==false)
-                {
-                     placed = true;
-                }
-            }
-        }
-    }
-    
-
-    private void FixedUpdate()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hits_ground, 1000, LayerMask))
-        {
-           position = hits_ground.point;
-        }   
-    }
-
-    float Gridsnap(float pos)
-    {
-        float rounding = pos % gridSize;
-        pos -= rounding;
-        if (rounding > (gridSize / 2))
-        {
-            pos += gridSize;
-        }
-
-        return pos;
-    }
+    public GameObject parent;
+    private GameObject objectloading;
     
     public void SelectObject(int index)
     {
-        if (object_loading != null)
+        if (objectloading != null)
         {
-            Destroy(object_loading);
+            if (objectloading.GetComponent<EditPlacedObjects>().IsSelected)
+            {
+                Destroy(objectloading);
+            }
         }
-        object_loading = Instantiate(objects[index], position, transform.rotation, Parent.transform);
-        
-    }
-
-    void UpdateColor()
-    {
-        if (canPlace)
-        {
-            object_loading.GetComponent<MeshRenderer>().material = showingplaceable[0];
-        }
-        else
-        {
-            object_loading.GetComponent<MeshRenderer>().material = showingplaceable[1];
-        }
+        objectloading = Instantiate(objects[index], new Vector3(0,0,0), objects[index].transform.rotation, parent.transform);
     }
 }
--- Assets/Script/Builder/EditPlacedObjects.cs
+++ Assets/Script/Builder/EditPlacedObjects.cs
@@ -1,51 +1,206 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
 
 public class EditPlacedObjects : MonoBehaviour
 {
-    public GameObject selectedObj;
-
-    void Start()
+    
+    public float gridsize;
+    private GameObject floor;
+    [Serializable]
+    public struct MaterialsList
     {
+        public Material wrongplacement;
+        public Material correctplacement;
+        public Material finish;
+    }
+    public MaterialsList materialslist;
+    private Outline _outline;
+    private bool _isselected; 
+    [HideInInspector]
+    public bool ismouving;
+    private Vector3 _position;
+    private CapsuleCollider _box;
+    [SerializeField] private LayerMask layermask;
+    private bool _canplace;
+    private Mesh _mesh;
+    private Vector3 _scale;
+    private Dictionary<Collider, Boolean> _canplaceobject = new Dictionary<Collider, Boolean>();
+    private bool _iscollinding = false;
+    
 
+    private bool _CanPlace
+    {
+        get { return _canplace;}
+        set
+        {
+            _canplace = value;
+            if (_canplace)
+            {
+                SetMaterial(materialslist.correctplacement);
+            }
+            else
+            {
+                SetMaterial(materialslist.wrongplacement);
+            }
+        }
+    }
+    public bool IsSelected
+    {
+        get {return _isselected;}
+        set
+        {
+            _isselected = value;
+            ismouving = value;
+            _outline.enabled = value;
+            if (!_isselected)
+            {
+                SetMaterial(materialslist.finish);
+            }
+        }
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        _outline = gameObject.AddComponent<Outline>();
+        _outline.OutlineColor = Color.black;
+        IsSelected = true;
+        _box = GetComponent<CapsuleCollider>();
+        floor = GameObject.Find("Floor");
+        _mesh = GetComponent<MeshFilter>().mesh;
+        _scale = RoundVector3(multiplyVector3byVector3(gameObject.transform.localScale, _mesh.bounds.size));
+    }
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000, layermask))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 1000))
+            _position = GridSnapCoordonate(hit.point);
+            _position.y = floor.transform.position.y;
+            if (IsSelected)
             {
-                if (hit.collider.gameObject.CompareTag("Object"))
+                gameObject.transform.position = _position;
+                if (_iscollinding)
                 {
-                    Select(hit.collider.gameObject);
+                    if (_canplaceobject.ContainsValue(false))
+                    {
+                        _CanPlace = false;
+                    }
+                    else if (_canplaceobject.ContainsValue(true))
+                    {
+                        _CanPlace = true;
+                    }
+                    Debug.Log("Number of collider" + _canplaceobject.Count);
+                }
+                else if (GameObject.FindGameObjectsWithTag("Object").Length == 1)
+                {
+                    _CanPlace = true;
+                }
+                else
+                {
+                    _CanPlace = false;
                 }
             }
-
         }
+    }
 
-        void Deselect()
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0) && _CanPlace)
         {
-            selectedObj.GetComponent<Outline>().enabled = false;
-            selectedObj = null;
+            IsSelected = !IsSelected;
         }
-   if (Input.GetMouseButtonDown(1))
-        {
-            Deselect();
-        }
+    }
 
-        void Select(GameObject obj)
+    private Vector3 GridSnapCoordonate(Vector3 pos)
+    {
+        return new Vector3(GridsnapValue(pos.x), GridsnapValue(pos.y), GridsnapValue(pos.z));
+    }
+
+    private int GridsnapValue(float pos)
+    {
+        return (int)Math.Round(pos);
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Object") && ismouving)
         {
-            if (obj == selectedObj) return;
-            if (selectedObj != null) Deselect();
-            Outline outline = obj.GetComponent<Outline>();
-            if (outline == null) obj.AddComponent<Outline>();
-            else outline.enabled = true;
-            selectedObj = obj;
-        } }
+            _canplaceobject[other] = IsValidePosition(other);
+            
+            Debug.Log(gameObject.name + IsValidePosition(other));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Object") && ismouving)
+        {
+            _canplaceobject.Remove(other);
+            if (_canplaceobject.Count == 0)
+            {
+                _iscollinding = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Object") && ismouving)
+        {
+            _iscollinding = true;
+        }
+    }
+
+    private bool InRange(float center1, float center2, float offset1, float offset2)
+    {
+        
+        Debug.Log("Offset: "+offset1);
+        Debug.Log("Min center 1: "+ (center1 - offset1));
+        Debug.Log("Max center 2: "+ (center2 + offset2));
+        Debug.Log("Max center 1: "+(center1 + offset2));
+        Debug.Log("Min center 2: "+ (center2 - offset1));
+        return center1 + offset1 > center2 - offset2 && center1 - offset1 < center2 + offset2;
+    }
+
+    private bool IsInside(Vector3 position1, Vector3 position2, Vector3 offset1, Vector3 offset2)
+    {
+        return InRange(position1.x, position2.x, offset1.x,offset2.x)&&
+               InRange(position1.y, position2.y, offset1.y,offset2.y)&&
+               InRange(position1.z, position2.z, offset1.z, offset2.z);
+    }
+
+    private Vector3 multiplyVector3byVector3(Vector3 a, Vector3 b)
+    {
+        return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
+    }
+
+    private void SetMaterial(Material material)
+    {
+        GetComponent<MeshRenderer>().material = material;
+    }
+
+    private bool IsValidePosition(Collider collider)
+    {
+        Mesh mesh = collider.gameObject.GetComponent<MeshFilter>().mesh;
+        Vector3 colliderscale =
+            RoundVector3(multiplyVector3byVector3(gameObject.transform.localScale, _mesh.bounds.size));
+        Debug.Log("Size: "+_scale);
+        Debug.Log("New collider");
+        if (IsInside(gameObject.transform.position, collider.transform.position, _scale / 2, colliderscale/2))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private Vector3 RoundVector3(Vector3 vector)
+    {
+        return new Vector3((float)Math.Round(vector.x), (float)Math.Round(vector.y), (float)Math.Round(vector.z));
+    }
 }
