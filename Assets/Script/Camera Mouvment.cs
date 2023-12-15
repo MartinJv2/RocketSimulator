235a47aae75f52b99961070886938c4488966ca2
MartinJ-Laptop <martinj@educ.college-montreal.qc.ca>
2023-12-15 10:46:13 -0500

    nOT DONE

--- Assets/Script/Camera Mouvment.cs
+++ Assets/Script/Camera Mouvment.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -14,9 +15,14 @@ public float speed;
 public float scale;
 public float minx;
 public float maxx;
-public float close;
-public float far;
+public float startingSize;
+public Quaternion startingRo;
 
+void Start()
+{
+    startingSize = size;
+    startingRo = Quaternion.Euler(new Vector3(x_rotation, y_rotation,gameObject.transform.rotation.z));
+}
 void Update()
 {
     if (Input.GetKey(KeyCode.A))
@@ -38,15 +44,24 @@ void Update()
 
     size += -Input.mouseScrollDelta.y * scale;
     
-    if (size < close || size > far)
-    {
-        size = close;
-    }
     gameObject.transform.rotation = Quaternion.Euler(new Vector3(x_rotation,y_rotation,transform.rotation.z));
     gameObject.transform.localScale = new Vector3(size, size, size);
 
-    
-    
+    if (GameObject.Find("physicManager").GetComponent<PhysiqueEngine>()._altitude > 10000)
+    {
+        startingSize = 1;
+        startingRo = Quaternion.Euler(new Vector3(-40, 0,0));
+    }
+}
+
+public void reset()
+{
+    gameObject.transform.localScale = new Vector3(startingSize, startingSize, startingSize);
+    gameObject.transform.rotation = Quaternion.Euler(new Vector3(startingRo.x, startingRo.y, startingRo.z));
+    size = startingSize;
+    x_rotation = startingRo.x;
+    y_rotation = startingRo.y;
+    Debug.Log("reset camera");
 }
 
 }
