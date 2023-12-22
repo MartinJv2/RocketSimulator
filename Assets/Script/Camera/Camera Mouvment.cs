c9fd70d73cde6c301a8fd20b075de49707ce81cd
Alexandre Cabana <cabanaA@educ.college-montreal.qc.ca>
2023-12-22 09:55:51 -0500

    Create camera prefab with a scriptable object to change value
    Add camera prefab on each scene and remove old camera
    Create camera prefab variant for the camera builder(no  follow gameobject component)
    Create a folder for the camera prefab

---
+++ Assets/Script/Camera/Camera Mouvment.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraMouvement : MonoBehaviour
+{
+    public CameraTransform cameratransform;
+    private float y_rotation;
+    private float x_rotation;
+    private float size;
+    public float speed;
+    public float scale;
+    public float minx;
+    public float maxx;
+    public float close;
+    public float far;
+
+    void Start()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(cameratransform.rotation);
+        gameObject.transform.localScale = cameratransform.scale;
+        y_rotation = cameratransform.rotation.y;
+        x_rotation = cameratransform.rotation.x;
+        size = gameObject.transform.localScale.x;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            y_rotation += speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            y_rotation -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            x_rotation -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            x_rotation += speed;
+        }
+
+        if (x_rotation > maxx)
+        {
+            x_rotation = maxx;
+        }
+        else if (x_rotation < minx)
+        {
+            x_rotation = minx;
+        }
+        if (y_rotation > 360)
+        {
+            y_rotation -= 360;
+        }
+        else if (y_rotation < 0)
+        {
+            y_rotation += 360;
+        }
+        size += -Input.mouseScrollDelta.y * scale;
+
+        if (size < close || size > far)
+        {
+            size = close;
+        }
+        cameratransform.rotation = new Vector3(x_rotation, y_rotation, transform.rotation.z);
+        gameObject.transform.rotation = Quaternion.Euler(cameratransform.rotation);
+        cameratransform.scale = new Vector3(size, size, size);
+        gameObject.transform.localScale = cameratransform.scale;
+    }
+}
