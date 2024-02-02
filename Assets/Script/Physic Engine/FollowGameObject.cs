8c3a89a8b7e2984cf538d0268415cb366623ba4c
MartinJ-Laptop <martinj@educ.college-montreal.qc.ca>
2024-02-01 21:14:11 -0500

    Speed Lines work

--- Assets/Script/Physic Engine/FollowGameObject.cs
+++ Assets/Script/Physic Engine/FollowGameObject.cs
@@ -6,12 +6,14 @@ public class FollowGameObject : MonoBehaviour
 {
     public string followgameobjectname;
     private GameObject _followgameobject;
+    public int shift;
     void Start()
     {
         _followgameobject = GameObject.Find(followgameobjectname);
     }
     void Update()
     {
-        gameObject.transform.position = new Vector3(_followgameobject.transform.position.x, _followgameobject.transform.position.y, _followgameobject.transform.position.z);
+        //gameObject.transform.position = new Vector3(_followgameobject.transform.position.x, _followgameobject.transform.position.y, _followgameobject.transform.position.z);
+        gameObject.transform.position = _followgameobject.transform.position + _followgameobject.transform.forward * shift;
     }
 }
