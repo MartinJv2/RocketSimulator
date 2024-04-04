de8c7168d9633e2c899ff6266b0c86a70116e6d3
MartinJ-Laptop <martinj@educ.college-montreal.qc.ca>
2024-04-04 10:41:29 -0400

    Add Menu at beginging

--- Assets/Script/Begin Game.cs
+++ Assets/Script/Begin Game.cs
@@ -11,7 +11,37 @@ public class BeginGame : MonoBehaviour
     public string builderscene;
     public string starterscene;
     public string tutorialScene;
-    
+    public GameObject menu;
+    public GameObject levels;
+    public Button CoinsButton;
+    public Button SpeedButton;
+
+    public void CoinLevels()
+    {
+
+        CoinsButton.GetComponent<Image>().color = new Color(.2f, .2f, .2f);
+        SpeedButton.GetComponent<Image>().color = new Color(100, 100, 100);
+    }
+    public void SpeedLevels()
+    {
+        SpeedButton.GetComponent<Image>().color = new Color(0f, 0f, 0f);
+        CoinsButton.GetComponent<Image>().color = new Color(.5f, .5f, .5f);
+    }
+    public void BackToBeginingOfLevelsMenu()
+    {
+        gameObject.SetActive(true);
+        levels.SetActive(false);
+    }
+    public void MenuStart()
+    {
+     menu.SetActive(true);
+     gameObject.SetActive(false);
+    }
+    public void SeeLevels()
+    {
+        menu.SetActive(false);
+        levels.SetActive(true);
+    }
     public void Begin()
     {
         SceneManager.LoadScene(builderscene, LoadSceneMode.Single);
